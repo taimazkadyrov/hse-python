651cf9192ce37e5657209416768ad533f3921cea
Kirill Yatsenko <kirillyat@gmail.com>
2025-09-06 00:10:44 +0300

    +

--- run_tests.sh
+++ run_tests.sh
@@ -4,5 +4,5 @@
 for task_dir in sem_01/task_*/; do
     echo "Running tests in $task_dir"
     # Переходим в директорию и запускаем тесты
-    (cd "$task_dir" && python -m pytest test.py -v)
+    (cd "$task_dir" && python -m pytest test.py -v || echo "Test failed in $task_dir")
 done
