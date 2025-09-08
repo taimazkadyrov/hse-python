d90e25e239ccc3f88f9200e6049cec17edbce388
Your Name <you@example.com>
2025-09-08 20:35:19 +0500

    Решение задания 01

--- task_01/solution.py
+++ task_01/solution.py
@@ -4,25 +4,25 @@
 
 def get_variables():
     # Целое число
-    integer_number = None
+    integer_number = 52
 
     # Число с плавающей точкой
-    float_number = None
+    float_number = 14.88
 
     # Строка
-    string_value = None
+    string_value = "Санкт-Петербург"
 
     # Логическое значение (истина)
-    boolean_true = None
+    boolean_true = True
 
     # Логическое значение (ложь)
-    boolean_false = None
+    boolean_false = False
 
     # Список из чисел от 1 до 5
-    list_of_numbers = None
+    list_of_numbers = [i for i in range(1, 6)]
 
     # Словарь с ключами 'name' и 'age', и соответствующими значениями
-    dictionary = None
+    dictionary = {"name": "Taimaz", "age": 18}
 
     # Вернуть все переменные в виде кортежа
     return (
