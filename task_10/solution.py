f528c9ab4e2cef49c3d03af55b21bdc70cf68478
Your Name <you@example.com>
2025-09-10 00:10:22 +0500

    Решение задания 10

--- task_10/solution.py
+++ task_10/solution.py
@@ -6,40 +6,39 @@ def create_set_from_list(lst):
     """
     Создает множество из списка, удаляя дубликаты
     """
-    # Ваш код здесь
-    pass
+    lf = set(lst)
+    return lf
 
 
 def is_subset(set1, set2):
     """
     Проверяет, является ли set1 подмножеством set2
     """
-    # Ваш код здесь
-    pass
+    return set1.issubset(set2)
 
 
 def union_sets(set1, set2):
     """
     Возвращает объединение двух множеств
     """
-    # Ваш код здесь
-    pass
+    set3 = set1.union(set2)
+    return set3
 
 
 def intersection_sets(set1, set2):
     """
     Возвращает пересечение двух множеств
     """
-    # Ваш код здесь
-    pass
+    set3 = set1.intersection(set2)
+    return set3
 
 
 def difference_sets(set1, set2):
     """
     Возвращает разность множеств (элементы, которые есть в set1, но отсутствуют в set2)
     """
-    # Ваш код здесь
-    pass
+    set3 = set1.difference(set2)
+    return set3
 
 
 def symmetric_difference_sets(set1, set2):
@@ -47,8 +46,8 @@ def symmetric_difference_sets(set1, set2):
     Возвращает симметричную разность множеств
     (элементы, которые есть в одном из множеств, но не в обоих одновременно)
     """
-    # Ваш код здесь
-    pass
+    set3 = set1.symmetric_difference(set2)
+    return set3
 
 
 def remove_duplicates_preserve_order(lst):
@@ -56,5 +55,10 @@ def remove_duplicates_preserve_order(lst):
     Удаляет дубликаты из списка, сохраняя порядок первого появления элементов
     Используйте множество для отслеживания уже встреченных элементов
     """
-    # Ваш код здесь
-    pass
+    set1 = set()
+    lst1 = list()
+    for item in lst:
+        if item not in set1:
+            lst1.append(item)
+            set1.add(item)
+    return lst1
